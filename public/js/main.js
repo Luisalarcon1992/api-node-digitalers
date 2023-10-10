cb491780a4e770776367a73c9ab51e5e7e6316ce
Luis Alarcon <luisalarcon1992@gmail.com>
2023-10-10 20:31:06 -0300

    front casi terminado

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,13 @@
-// eslint-disable-next-line no-undef
+// eslint-disable-next-line no-undef, no-unused-vars
 const swiper = new Swiper('.mySwiper', {
   slidesPerView: 1,
   centeredSlides: true,
   loop: true,
-  spaceBetween: 30,
+  spaceBetween: 10,
   grabCursor: true,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '#swiper-button-next',
+    prevEl: '#swiper-button-prev',
   },
   breakpoints: {
     991: {
@@ -16,4 +16,18 @@ const swiper = new Swiper('.mySwiper', {
   },
 });
 
-swiper();
+// eslint-disable-next-line no-undef, no-unused-vars
+const swiper2 = new Swiper('.mySwiper2', {
+  slidesPerView: 1,
+  loop: true,
+  grabCursor: true,
+  navigation: {
+    nextEl: '#swiper-button-next2',
+    prevEl: '#swiper-button-prev2',
+  },
+  breakpoints: {
+    991: {
+      slidesPerView: 2,
+    },
+  },
+});
